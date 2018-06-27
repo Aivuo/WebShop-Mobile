e3d5d1fef543128af682cffe4a7111a016f1138c
Aivuo <rioneda1@gmail.com>
2018-06-27 14:29:05 +0200

    Database In

---
+++ WebShop Mobile/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop_Mobile.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderDate { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public List<OrderRow> OrderRows { get; set; }
+
+        public Order()
+        {
+            OrderRows = new List<OrderRow>();
+        }
+
+    }
+}
---
+++ WebShop Mobile/Models/WebShopMobileDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebShop_Mobile.Models
+{
+    public class WebShopMobileDb : DbContext
+    {
+        
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CellPhone> CellPhones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderRow> OrderRows { get; set; }
+
+    }
+}
