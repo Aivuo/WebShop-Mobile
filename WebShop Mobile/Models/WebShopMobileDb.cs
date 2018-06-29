8801fddab0e67800f6bf1c1168acabdf29d346fc
Aivuo <rioneda1@gmail.com>
2018-06-29 10:38:23 +0200

    Mysql upp and running

--- WebShop Mobile/Models/WebShopMobileDb.cs
+++ WebShop Mobile/Models/WebShopMobileDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.Entity;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Web;
 
 namespace WebShop_Mobile.Models
 {
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class WebShopMobileDb : DbContext
     {
         
