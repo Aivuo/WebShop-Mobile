6435a1f973feae2efeb0a310912a8b48d675ee8b
Aivuo <rioneda1@gmail.com>
2018-07-11 11:18:27 +0200

    Shopping history implemented

---
+++ WebShop Mobile/Models/IndexUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop_Mobile.Models
+{
+    public class IndexUserViewModel
+    {
+            public IndexViewModel indexViewModel;
+            public UserViewModel userViewModel;
+
+            public IndexUserViewModel(IndexViewModel indexViewModel, UserViewModel userViewModel)
+            {
+                this.indexViewModel = indexViewModel;
+                this.userViewModel = userViewModel;
+            }
+    }
+}
