c182a608449b43f7909653cfbecf62fece522aef
Aivuo <rioneda1@gmail.com>
2018-07-17 15:50:23 +0200

    Pdf Receipt

---
+++ WebShop Mobile/Controllers/PdfController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShop_Mobile.Models;
+
+namespace WebShop_Mobile.Controllers
+{
+    public class PdfController : Controller
+    {
+        // GET: Pdf
+        public ActionResult Receipt(int orderId, string payment, float total)
+        {
+            WebShopMobileDb Db = new WebShopMobileDb();
+            var user = User.Identity.Name;
+
+            var model = Methods.FindCartOrder(user);
+            var customer = Db.Customers.FirstOrDefault(x => x.EmailAdress == user);
+
+            var model2 = Db.Orders.FirstOrDefault(x => x.Id == model.Id);
+            model2.Processed = true;
+            Db.SaveChanges();
+
+            model.OrderDate = DateTime.Now.ToShortDateString();
+            var reciept = new RecieptViewModel(model, customer, payment, total);
+
+            return Pdf(reciept);
+        }
+
+        protected ActionResult Pdf()
+        {
+            return Pdf(null, null, null);
+        }
+
+        protected ActionResult Pdf(string fileDownloadName)
+        {
+            return Pdf(fileDownloadName, null, null);
+        }
+
+        protected ActionResult Pdf(string fileDownloadName, string viewName)
+        {
+            return Pdf(fileDownloadName, viewName, null);
+        }
+
+        protected ActionResult Pdf(object model)
+        {
+            return Pdf(null, null, model);
+        }
+
+        protected ActionResult Pdf(string fileDownloadName, object model)
+        {
+            return Pdf(fileDownloadName, null, model);
+        }
+
+        protected ActionResult Pdf(string fileDownloadName, string viewName, object model)
+        {
+            // Based on View() code in Controller base class from MVC
+            if (model != null)
+            {
+                ViewData.Model = model;
+            }
+            PdfResult pdf = new PdfResult()
+            {
+                FileDownloadName = fileDownloadName,
+                ViewName = viewName,
+                ViewData = ViewData,
+                TempData = TempData,
+                ViewEngineCollection = ViewEngineCollection
+            };
+            return pdf;
+        }
+    }
+}
---
+++ WebShop Mobile/Models/RecieptViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop_Mobile.Models
+{
+    public class RecieptViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public string BillingAdress { get; set; }
+        public string BillingZip { get; set; }
+        public string BillingCity { get; set; }
+
+        public string DeliveryAdress { get; set; }
+        public string DeliveryZip { get; set; }
+        public string DeliveryCity { get; set; }
+
+        public string EmailAdress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string OrderDate { get; set; }
+        public List<OrderRow> OrderRows { get; set; }
+
+        public string PaymentOption { get; set; }
+
+        public float Total { get; set; }
+
+        public RecieptViewModel()
+        {
+            OrderRows = new List<OrderRow>();
+        }
+
+        public RecieptViewModel(Order order, Customer customer, string payment, float total)
+        {
+            Firstname = customer.Firstname;
+            Lastname = customer.Lastname;
+
+            BillingAdress = customer.BillingAdress;
+            BillingCity = customer.BillingCity;
+            BillingZip = customer.BillingZip;
+
+            DeliveryAdress = customer.DeliveryAdress;
+            DeliveryCity = customer.DeliveryCity;
+            DeliveryZip = customer.DeliveryZip;
+
+            EmailAdress = customer.EmailAdress;
+            PhoneNumber = customer.PhoneNumber;
+
+            OrderDate = order.OrderDate;
+            OrderRows = new List<OrderRow>();
+
+            foreach (var item in order.OrderRows)
+            {
+                OrderRows.Add(item);
+            }
+
+            PaymentOption = payment;
+
+            Total = total;
+        }
+    }
+}
